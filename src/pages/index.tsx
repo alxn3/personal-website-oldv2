5723270930203972984c873566564ad54494c3ac
Alex Lin <38541529+alxn3@users.noreply.github.com>
2022-03-31 17:22:29 -0400

    Add default styling and colors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,44 @@
 const Home = () => {
-  return <div className="text-6xl font-extrabold">Hello World!</div>;
+  return (
+    <div className="h-full grid grid-cols-3">
+      <div className="flex flex-col h-full w-full">
+        <div className="h-full w-full bg-primary-50"></div>
+        <div className="h-full w-full bg-primary-100"></div>
+        <div className="h-full w-full bg-primary-200"></div>
+        <div className="h-full w-full bg-primary-300"></div>
+        <div className="h-full w-full bg-primary-400"></div>
+        <div className="h-full w-full bg-primary-500"></div>
+        <div className="h-full w-full bg-primary-600"></div>
+        <div className="h-full w-full bg-primary-700"></div>
+        <div className="h-full w-full bg-primary-800"></div>
+        <div className="h-full w-full bg-primary-900"></div>
+      </div>
+      <div className="flex flex-col h-full w-full">
+        <div className="h-full w-full bg-secondary-50"></div>
+        <div className="h-full w-full bg-secondary-100"></div>
+        <div className="h-full w-full bg-secondary-200"></div>
+        <div className="h-full w-full bg-secondary-300"></div>
+        <div className="h-full w-full bg-secondary-400"></div>
+        <div className="h-full w-full bg-secondary-500"></div>
+        <div className="h-full w-full bg-secondary-600"></div>
+        <div className="h-full w-full bg-secondary-700"></div>
+        <div className="h-full w-full bg-secondary-800"></div>
+        <div className="h-full w-full bg-secondary-900"></div>
+      </div>
+      <div className="flex flex-col h-full w-full">
+        <div className="h-full w-full bg-accent-50"></div>
+        <div className="h-full w-full bg-accent-100"></div>
+        <div className="h-full w-full bg-accent-200"></div>
+        <div className="h-full w-full bg-accent-300"></div>
+        <div className="h-full w-full bg-accent-400"></div>
+        <div className="h-full w-full bg-accent-500"></div>
+        <div className="h-full w-full bg-accent-600"></div>
+        <div className="h-full w-full bg-accent-700"></div>
+        <div className="h-full w-full bg-accent-800"></div>
+        <div className="h-full w-full bg-accent-900"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,63 @@
+const withOpacityValue = (hue, hueShift, saturation, luminiosity) => {
+  return ({ opacityValue }) => {
+    if (opacityValue === undefined) {
+      return `hsl(calc(var(${hue}) + ${hueShift}), ${saturation}%, ${luminiosity}%)`;
+    }
+    return `hsl(calc(var(${hue}) + ${hueShift}), ${saturation}%, ${luminiosity}%, ${opacityValue})`;
+  };
+};
+
+const COLOR_PALETTE_VALUES = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
+const generateSwatch = (
+  hue,
+  settings = {
+    hueShift: [-10, 10],
+    saturation: [50, 90],
+    luminiosity: [10, 90],
+  }
+) => {
+  const rangeLum = settings.luminiosity[1] - settings.luminiosity[0];
+  const stepLum = rangeLum / COLOR_PALETTE_VALUES.length;
+  const rangeHue = settings.hueShift[1] - settings.hueShift[0];
+  const stepHue = rangeHue / COLOR_PALETTE_VALUES.length;
+  const rangeSat = settings.saturation[1] - settings.saturation[0];
+  const stepSat = rangeSat / COLOR_PALETTE_VALUES.length;
+  return Object.fromEntries(
+    Array.from(COLOR_PALETTE_VALUES, (elem, i) => [
+      elem,
+      withOpacityValue(
+        hue,
+        settings.hueShift[1] - i * stepHue,
+        settings.saturation[1] - i * stepSat,
+        settings.luminiosity[1] - i * stepLum
+      ),
+    ])
+  );
+};
+
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  darkMode: 'class',
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: generateSwatch('--color-primary', {
+          hueShift: [-10, 10],
+          saturation: [40, 90],
+          luminiosity: [10, 90],
+        }),
+        secondary: generateSwatch('--color-secondary', {
+          hueShift: [-10, 20],
+          saturation: [40, 90],
+          luminiosity: [10, 90],
+        }),
+        accent: generateSwatch('--color-accent', {
+          hueShift: [-10, 30],
+          saturation: [40, 90],
+          luminiosity: [10, 90],
+        }),
+      },
+    },
   },
   plugins: [],
-}
+};
