ff7dd47fa227fa8c8e2b5c147f83d85c4c8b5ad3
Alex Lin <38541529+alxn3@users.noreply.github.com>
2022-04-01 18:59:26 -0400

    Adjust styles and colors

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <div className="flex flex-col items-center justify-center h-full">
       <div className="text-[min(14vw,7rem)] leading-none">
         <div className="mb-12 space-y-2">
-          <p className="text-[0.3em] font-bold text-accent-800 dark:text-accent-300">
+          <p className="text-[0.3em] font-bold text-accent-300">
             Hello there.
           </p>
           <h1 className="font-semibold text-primary-200 dark:text-primary-700">
@@ -15,10 +15,10 @@ const Hero = () => {
               Alex Lin
             </span>
           </h1>
-          <h2 className="text-[0.5em] text-secondary-900 dark:text-secondary-300">
+          <h2 className="text-[0.5em] text-secondary-600 dark:text-secondary-300">
             Lorem, ipsum dolor.
           </h2>
-          <p className="text-[0.25em] font-normal text-slate-900/80 dark:text-slate-200/80 leading-[1.1em] py-4">
+          <p className="text-[0.25em] font-normal leading-[1.1em] py-4">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
             corrupti repellat sit adipisci dignissimos non eveniet.
           </p>
