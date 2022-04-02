958d39a6f42e48308472d71de144f086026211e2
Alex Lin <38541529+alxn3@users.noreply.github.com>
2022-04-02 04:07:25 -0400

    Make text bigger on smaller screens

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -4,26 +4,26 @@ import Link from 'next/link';
 const Hero = () => {
   return (
     <div className="flex flex-col items-center justify-center h-full">
-      <div className="text-[min(14vw,7rem)] leading-none">
+      <div className="text-[max(3rem,min(14vw,7rem))] leading-none">
         <div className="mb-12 space-y-2">
-          <p className="text-[0.3em] font-bold text-accent-600 dark:text-accent-300">
+          <p className="text-[max(1.2rem,0.3em)] font-bold text-accent-600 dark:text-accent-300">
             Hello there.
           </p>
           <h1 className="font-semibold text-primary-200 dark:text-primary-600">
             I&apos;m{' '}
-            <span className="font-bold text-primary-800 dark:text-primary-100">
+            <span className="inline-block font-bold text-primary-800 dark:text-primary-100">
               Alex Lin
             </span>
           </h1>
           <h2 className="text-[0.5em] text-secondary-600 dark:text-secondary-300">
             Lorem, ipsum dolor.
           </h2>
-          <p className="text-[0.25em] font-normal leading-[1.1em] py-4">
+          <p className="text-[max(1.2rem,0.25em)] font-normal leading-[1.1em] py-4">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
             corrupti repellat sit adipisci dignissimos non eveniet.
           </p>
         </div>
-        <OutlineButton className="block leading-normal text-[0.25em]">
+        <OutlineButton className="block leading-normal text-[max(1.2rem,0.25em)]">
           <Link href="/">Learn more.</Link>
         </OutlineButton>
       </div>
